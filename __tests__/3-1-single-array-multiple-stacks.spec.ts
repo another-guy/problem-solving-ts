e86d40a9e6c0102f518c0c8f42b509b7049721cd
Igor Soloydenko <soloydenko@gmail.com>
2018-05-04 16:16:06 -0700

    3.1 Add DoubleStack

--- __tests__/3-1-single-array-multiple-stacks.spec.ts
+++ __tests__/3-1-single-array-multiple-stacks.spec.ts
@@ -1,4 +1,177 @@
-import { SingleStack } from '../src/cracking-the-coding-interview/3-stacks-and-queues/3-1-single-array-multiple-stacks';
+import {
+  DoubleStack,
+  SingleStack,
+} from '../src/cracking-the-coding-interview/3-stacks-and-queues/3-1-single-array-multiple-stacks';
+
+describe(DoubleStack.name, () => {
+  describe(`left stack`, () => {
+    [
+      { capacity: null, values: [ 1 ] },
+      { capacity: null, values: [ 1, 2 ] },
+      { capacity: null, values: [ 1, 2, 3 ] },
+    ].forEach(testCase => {
+      it(`Should correctly implement pushLeft(), popLeft(), isEmpty* for ${JSON.stringify(testCase)}`, () => {
+        const { capacity, values } = testCase;
+  
+        const stack = capacity ? new DoubleStack(capacity) : new DoubleStack();
+        expect(stack.isEmpty).toBeTruthy();
+    
+        values.forEach(value => stack.pushLeft(value));
+        expect(stack.isEmpty).toBeFalsy();
+        expect(stack.isRightEmpty).toBeTruthy();
+    
+        for (let index = values.length - 1; index >= 0; index--) {
+          const popped = stack.popLeft();
+          expect(popped).toEqual(values[index]);
+        }
+        expect(stack.isEmpty).toBeTruthy();  
+      });
+    });
+  
+    [
+      { capacity: null, values: [ ] },
+    ].forEach(testCase => {
+      it(`Should correctly implement pushLeft(), popLeft(), isEmpty* for ${JSON.stringify(testCase)}`, () => {
+        const { capacity, values } = testCase;
+  
+        const stack = capacity ? new DoubleStack(capacity) : new DoubleStack();
+        expect(stack.isEmpty).toBeTruthy();
+    
+        values.forEach(value => stack.pushLeft(value));
+        expect(stack.isEmpty).toBeTruthy();
+      });
+    });
+  
+    [
+      { capacity: 0 },
+      { capacity: 1 },
+      { capacity: 2 },
+    ].forEach(testCase => {
+      it(`Should throw error when pushLeft() is invoked on full stack of capacity ${JSON.stringify(testCase)}`, () => {
+        const { capacity } = testCase;
+  
+        const stack = new DoubleStack(capacity);
+        expect(stack.isEmpty).toBeTruthy();
+    
+        new Array(capacity)
+          .fill(null)
+          .forEach((_, index) => stack.pushLeft(index));
+        
+        expect(() => stack.pushLeft(100)).toThrow();
+      });
+    });  
+  });
+
+  describe(`right stack`, () => {
+    [
+      { capacity: null, values: [ 1 ] },
+      { capacity: null, values: [ 1, 2 ] },
+      { capacity: null, values: [ 1, 2, 3 ] },
+    ].forEach(testCase => {
+      it(`Should correctly implement pushRight(), popRight(), isEmpty* for ${JSON.stringify(testCase)}`, () => {
+        const { capacity, values } = testCase;
+
+        const stack = capacity ? new DoubleStack(capacity) : new DoubleStack();
+        expect(stack.isEmpty).toBeTruthy();
+    
+        values.forEach(value => stack.pushRight(value));
+        expect(stack.isEmpty).toBeFalsy();
+        expect(stack.isLeftEmpty).toBeTruthy();
+    
+        for (let index = values.length - 1; index >= 0; index--) {
+          const popped = stack.popRight();
+          expect(popped).toEqual(values[index]);
+        }
+        expect(stack.isEmpty).toBeTruthy();  
+      });
+    });
+
+    [
+      { capacity: null, values: [ ] },
+    ].forEach(testCase => {
+      it(`Should correctly implement pushRight(), popRight(), isEmpty* for ${JSON.stringify(testCase)}`, () => {
+        const { capacity, values } = testCase;
+
+        const stack = capacity ? new DoubleStack(capacity) : new DoubleStack();
+        expect(stack.isEmpty).toBeTruthy();
+    
+        values.forEach(value => stack.pushRight(value));
+        expect(stack.isEmpty).toBeTruthy();
+      });
+    });
+
+    [
+      { capacity: 0 },
+      { capacity: 1 },
+      { capacity: 2 },
+    ].forEach(testCase => {
+      it(`Should throw error when pushRight() is invoked on full stack of capacity ${JSON.stringify(testCase)}`, () => {
+        const { capacity } = testCase;
+
+        const stack = new DoubleStack(capacity);
+        expect(stack.isEmpty).toBeTruthy();
+    
+        new Array(capacity)
+          .fill(null)
+          .forEach((_, index) => stack.pushRight(index));
+        
+        expect(() => stack.pushRight(100)).toThrow();
+      });
+    });
+  });
+
+  describe(`simultaneous stacks usage`, () => {
+    it(`should pass an end-to-end test`, () => {
+      const stack = new DoubleStack(5);
+      expect(stack.isEmpty).toBeTruthy();
+
+      stack.pushLeft(1);
+      stack.pushRight(5);
+      stack.pushLeft(2);
+      stack.pushRight(4);
+      stack.pushLeft(3);
+
+      expect(stack.isLeftEmpty).toBeFalsy();
+      expect(stack.isRightEmpty).toBeFalsy();
+
+      expect(stack.popLeft()).toBe(3);
+      expect(stack.popLeft()).toBe(2);
+      expect(stack.popLeft()).toBe(1);
+      expect(stack.popRight()).toBe(4);
+      expect(stack.popRight()).toBe(5);
+
+      expect(stack.isEmpty).toBeTruthy();
+      expect(stack.isLeftEmpty).toBeTruthy();
+      expect(stack.isRightEmpty).toBeTruthy();
+    });
+
+    it(`pushRight() should throw when violates lefts's boundaries`, () => {
+      const stack = new DoubleStack(5);
+      expect(stack.isEmpty).toBeTruthy();
+
+      stack.pushLeft(1);
+      stack.pushRight(5);
+      stack.pushLeft(2);
+      stack.pushRight(4);
+      stack.pushLeft(3);
+
+      expect(() => stack.pushRight(3)).toThrow();
+    });
+    
+    it(`pushLeft() should throw when violates right's boundaries`, () => {
+      const stack = new DoubleStack(5);
+      expect(stack.isEmpty).toBeTruthy();
+
+      stack.pushLeft(1);
+      stack.pushRight(5);
+      stack.pushLeft(2);
+      stack.pushRight(4);
+      stack.pushRight(3);
+
+      expect(() => stack.pushLeft(3)).toThrow();
+    });
+  });
+});
 
 describe(SingleStack.name, () => {
   [
--- src/cracking-the-coding-interview/3-stacks-and-queues/3-1-single-array-multiple-stacks.ts
+++ src/cracking-the-coding-interview/3-stacks-and-queues/3-1-single-array-multiple-stacks.ts
@@ -2,12 +2,69 @@
 3.1 Describe how you could use a single array to implement three stacks.
 */
 
+export const DEFAULT_STACK_CAPACITY = 1024;
+
+export class DoubleStack {
+  data: number[];
+  topLeftIndex: number;
+  topRightIndex: number;
+
+  constructor(capacity: number = DEFAULT_STACK_CAPACITY) {
+    this.data = new Array(capacity).fill(null);
+    this.topLeftIndex = -1;
+    this.topRightIndex = this.data.length;
+  }
+
+  get isEmpty(): boolean { return this.isLeftEmpty && this.isRightEmpty; }
+  get isLeftEmpty(): boolean { return this.topLeftIndex === -1; }
+  get isRightEmpty(): boolean { return this.topRightIndex === this.data.length; }
+
+  pushLeft(value: number): void {
+    if (this.unsafeToPush())
+      throw new Error(`No space for push`);
+
+    this.topLeftIndex++;
+    this.data[this.topLeftIndex] = value;
+  }
+
+  popLeft(): number {
+    if (this.isLeftEmpty)
+      throw new Error(`Left stack is empty.`)
+
+    const result = this.data[this.topLeftIndex];
+    this.topLeftIndex--;
+    return result;
+  }
+
+  pushRight(value: number): void {    
+    if (this.unsafeToPush())
+      throw new Error(`No space for push`);
+
+    this.topRightIndex--;
+    this.data[this.topRightIndex] = value;
+  }
+
+  popRight(): number {
+    if (this.isRightEmpty)
+      throw new Error(`Right stack is empty.`);
+    
+    const result = this.data[this.topRightIndex];
+    this.topRightIndex++;
+    return result;
+  }
+
+  private unsafeToPush(): boolean {
+    return this.topLeftIndex + 1 === this.topRightIndex;
+  }
+}
+
 export class SingleStack {
   data: number[];
-  topIndex: number = -1;
+  topIndex: number;
 
-  constructor(capacity: number = 1024) {
+  constructor(capacity: number = DEFAULT_STACK_CAPACITY) {
     this.data = new Array(capacity).fill(null);
+    this.topIndex = -1;
   }
 
   get isEmpty(): boolean {
